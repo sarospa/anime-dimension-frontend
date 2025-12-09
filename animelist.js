6dcd032cc250747696a1845ad3b8db205c36ca06
June Cantor <ffivfan@hotmail.com>
2025-12-09 18:04:12 -0500

    Implemented a generalized anime search tool

--- animelist.js
+++ animelist.js
@@ -7,9 +7,21 @@ displayRows = {
 	"Source": {displayName: "Source", width: "5%"},
 	"Priority": {displayName: "Priority", width: "5%"}
 }
+queryColumns = {
+	"AnimeId": "text",
+	"Title": "text",
+	"Review": "text",
+	"Notes": "text",
+	"YuriRatingId": [[0, "No yuri"], [1, "Little yuri"], [2, "Weak yuri"], [3, "Strong yuri"], [4, "Explicit yuri"], [5, "Very explicit yuri"], [6, "Overwhelming yuri"]],
+	"ReleaseDate": "text",
+	"LastEpisode": "text",
+	"Priority": [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10]],
+	"Completion": [[0, "New"], [1, "Started"], [2, "One season"], [3, "All main episodes"], [4, "Everything"]],
+}
 columns = [];
 data = [];
 partnersList = [];
+queryCount = 0;
 
 $.when($.ready).then(async function() {
 	try {
@@ -29,12 +41,30 @@ $.when($.ready).then(async function() {
 		
 		const partnersResult = await partnersResponse.json();
 		partnersList = partnersResult["message"]["rows"];
+		queryColumns["WatchPartners"] = partnersList;
+		queryColumns["WatchPartnersActive"] = partnersList;
 		
 		let partnersDropdown = $("#partnerSearch");
 		for (let row = 0; row < partnersList.length; row++) {
 			let rowData = partnersList[row];
 			partnersDropdown.append($(`<option value=${rowData[0]}>${rowData[1]}</option>`));
 		}
+		
+		const sourcesResponse = await fetch(`${baseURL}/sources`);
+		if (!sourcesResponse.ok) {
+		  throw new Error(`Response status: ${sourcesResponse.status}`);
+		}
+		
+		const sources = await sourcesResponse.json();
+		queryColumns["SourceId"] = sources["message"]["rows"];
+		
+		const tagsResponse = await fetch(`${baseURL}/tags`);
+		if (!tagsResponse.ok) {
+			throw new Error(`Response status: ${tagsResponse.status}`);
+		}
+		
+		const tagsData = await tagsResponse.json();
+		queryColumns["TagIds"] = tagsData["message"]["rows"];
 		
 		buildTable();
 	} catch (error) {
@@ -46,7 +76,7 @@ function find(row, column) {
 	return row[columns.findIndex((col) => col === column)]
 }
 
-function buildTable(titleSearch, partnerId, reviewPending) {
+function buildTable(titleSearch, partnerId, reviewPending, queries) {
 	let table = $("#animetable");
 	table.empty();
 	
@@ -67,7 +97,8 @@ function buildTable(titleSearch, partnerId, reviewPending) {
 		let activePartners = find(data[row], "WatchPartnersActive")?.split(",");
 		if ((!titleSearch || find(data[row], "Title").toLowerCase().includes(titleSearch)) &&
 			(!partnerId || activePartners?.includes(partnerId)) &&
-			(!reviewPending || (!find(data[row], "Review") && find(data[row], "Completion") >= 2))) {
+			(!reviewPending || (!find(data[row], "Review") && find(data[row], "Completion") >= 2)) &&
+			foundInQuerySearch(data[row], queries)) {
 			let parity = isEven ? "even" : "odd"
 			isEven = !isEven;
 			let rowElem = $(`<tr class='${parity}'>`);
@@ -106,4 +137,99 @@ async function navigateToRandomAnime() {
 
 async function downloadBackup() {
 	window.location.href = `${baseURL}/backup`;
+}
+
+function updateQueryValueField(queryNumber) {
+	let column = $(`#query-${queryNumber} .columnDropdown`).val();
+	if (queryColumns[column] == "text") {
+		$(`#query-${queryNumber} .queryValue`).replaceWith($("<input type='text' class='queryValue' onkeyup='querySearch()' />"));
+	}
+	else if (queryColumns[column]) {
+		let newQueryValue = $(`<select class='queryValue' onchange='querySearch()'></select`);
+		for (let i = 0; i < queryColumns[column].length; i++) {
+			newQueryValue.append($(`<option value=${queryColumns[column][i][0]}>${queryColumns[column][i][1]}</option>`))
+		}
+		$(`#query-${queryNumber} .queryValue`).replaceWith(newQueryValue);
+	}
+	querySearch();
+}
+
+function querySearch() {
+	let dropdownDiv = $("#queryDropdowns");
+	let queries = [];
+	for (let i = 0; i < queryCount; i++) {
+		let column = $(`#query-${i} .columnDropdown`).val();
+		let operator = $(`#query-${i} .opDropdown`).val();
+		let value = $(`#query-${i} .queryValue`).val();
+		queries.push([column, operator, value]);
+	}
+	buildTable(null, null, null, queries)
+}
+
+function foundInQuerySearch(row, queries) {
+	if (!queries) return true;
+	let found = true;
+	for (let i = 0; i < queries.length; i++) {
+		let query = queries[i];
+		let rowValue = find(row, query[0])?.toString().toLowerCase();
+		let operator = query[1];
+		let value = query[2].toLowerCase();
+		let rowArr = rowValue?.split(",");
+		switch(operator) {
+			case "IS":
+				if (!(rowValue == value)) found = false;
+				break;
+			case "NOT":
+				if (!(rowValue != value)) found = false;
+				break;
+			case "<":
+				if (!(rowValue < value)) found = false;
+				break;
+			case "<=":
+				if (!(rowValue <= value)) found = false;
+				break;
+			case ">":
+				if (!(rowValue > value)) found = false;
+				break;
+			case ">=":
+				if (!(rowValue >= value)) found = false;
+				break;
+			case "MATCHES":
+				if (!rowValue.includes(value)) found = false;
+				break;
+			case "HAS":
+				if (!rowArr || !rowArr.includes(value)) found = false;
+				break;
+			case "NOT HAS":
+				if (rowArr && rowArr.includes(value)) found = false;
+				break;
+		}
+	}
+	return found;
+}
+
+async function addQuery() {
+	let dropdownDiv = $("#queryDropdowns");
+	let newSpan = $(`<div id='query-${queryCount}'></div>`);
+	let colDropdown = $(`<select class='columnDropdown' onchange='updateQueryValueField(${queryCount})'></select>`)
+	for (let i = 0; i < columns.length; i++) {
+		if (queryColumns[columns[i]]) {
+			colDropdown.append($(`<option value='${columns[i]}'>${columns[i]}</option>`));
+		}
+	}
+	newSpan.append(colDropdown);
+	newSpan.append($(`<select class='opDropdown' onchange='querySearch()'>
+			<option value='IS'>IS</option>
+			<option value='NOT'>NOT</option>
+			<option value='&lt;'>&lt;</option>
+			<option value='&lt;='>&lt;=</option>
+			<option value='&gt;'>&gt;</option>
+			<option value='&gt;='>&gt;=</option>
+			<option value='MATCHES'>MATCHES</option>
+			<option value='HAS'>HAS</option>
+			<option value='NOT HAS'>NOT HAS</option>
+		</select>`));
+	newSpan.append($("<input type='text' class='queryValue' onkeyup='querySearch()' />"));
+	dropdownDiv.append(newSpan);
+	queryCount++;
 }
